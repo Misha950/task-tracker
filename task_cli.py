d2c56fc7995b37bdbc253cacb5fcd0931076f6b5
Misha <Misha2006az@gmail.com>
2026-04-16 18:45:29 +0300

    Add load_tasks and save_tasks functions

--- task_cli.py
+++ task_cli.py
@@ -4,3 +4,15 @@ import os
 from datetime import datetime
 
 TASKS_FILE = "tasks.json"
+
+
+def load_tasks():
+    if not os.path.exists(TASKS_FILE):
+        return []
+    with open(TASKS_FILE, "r") as file:
+        return json.load(file)
+
+
+def save_tasks(tasks):
+    with open(TASKS_FILE, "w", encoding="utf-8") as f:
+        json.dump(tasks, f, ensure_ascii=False, indent=2)
